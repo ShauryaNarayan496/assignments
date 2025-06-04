ca55c35b347deca7d24a9121c1d2ea988f131adb
shaurya <shauryanarayan@Yashs-MacBook-Air-2.local>
2025-06-04 17:05:23 +0530

    second commit

--- each_char.py
+++ each_char.py
@@ -1,4 +1,12 @@
-def each_char(str):
-    for each_char in str:
-        count = 0
-        
+def count_characters(str):
+    #create a dictionary to store the count of each character
+    char_count = {}
+    for char in str:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+str = input()
+print(count_characters(str))
